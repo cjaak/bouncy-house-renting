de7ab0123d992cb35001a40b45ee869059b30a16
charlie wiegand <chwiegand@proton.me>
2022-10-19 15:23:00 +0200

    feat: call users api

--- admin-panel/src/app/data-access/services/user.service.ts
+++ admin-panel/src/app/data-access/services/user.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import {User} from "../../shared/models/user.model";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, tap, throwError} from "rxjs";
+import {CustomResponse} from "../../shared/interfaces/custom-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private readonly apiUrl = "http://localhost:8080"
+
+  users$ = <Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/user`).pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
 
 
-
-  getAllUsers(){
-    return {};
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(() => new Error(`An error occurred - Error code ${error.status}`));
   }
 
-  getUser(id: string){
-
-  }
 
 }
