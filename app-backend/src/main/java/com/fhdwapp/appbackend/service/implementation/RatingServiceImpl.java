6fbf11a2c16a01ea76727e1d0a8c25a282c42ec0
charlie wiegand <chwiegand@proton.me>
2022-11-10 23:05:31 +0100

    feat: implement service

--- app-backend/src/main/java/com/fhdwapp/appbackend/service/implementation/RatingServiceImpl.java
+++ app-backend/src/main/java/com/fhdwapp/appbackend/service/implementation/RatingServiceImpl.java
@@ -1,48 +1,68 @@
 package com.fhdwapp.appbackend.service.implementation;
 
 import com.fhdwapp.appbackend.model.Rating;
+import com.fhdwapp.appbackend.repo.RatingRepo;
 import com.fhdwapp.appbackend.service.RatingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class RatingServiceImpl implements RatingService {
+
+    private final RatingRepo ratingRepo;
     @Override
     public Collection<Rating> getAll() {
-        return null;
+        log.info("Fetching all ratings");
+        return ratingRepo.findAll();
     }
 
     @Override
     public Collection<Rating> getAllByUserId(Long userId) {
-        return null;
+        log.info("Fetching all ratings by userId");
+        return ratingRepo.findAllByUserId(userId);
     }
 
     @Override
     public Collection<Rating> getAllByBouncyHouseId(Long bouncyHouseId) {
-        return null;
+        log.info("Fetching all ratings by bouncyHouseId");
+        return ratingRepo.findAllByBouncyHouseId(bouncyHouseId);
     }
 
     @Override
     public Rating getByRentedId(Long rentedId) {
-        return null;
+        log.info("Fetching  rating by rentedId");
+        return ratingRepo.findByRentedId(rentedId);
     }
 
     @Override
     public Rating get(Long id) {
-        return null;
+        log.info("Fetching  rating");
+        return ratingRepo.findById(id).get();
     }
 
     @Override
     public Rating create(Rating rating) {
-        return null;
+        log.info("Creating rating");
+        return ratingRepo.save(rating);
     }
 
     @Override
     public Rating update(Rating rating) {
-        return null;
+        log.info("Updating rating");
+        return ratingRepo.save(rating);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting rating");
+        ratingRepo.deleteById(id);
+        return true;
     }
 }
